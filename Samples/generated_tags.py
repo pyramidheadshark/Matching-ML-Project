c0e84048f3bf56af9601d1da16f9c02c53b2d679
pyramidheadshark <notevengael@gmail.com>
2024-02-28 15:44:59 +0300

    test

---
+++ Samples/generated_tags.py
@@ -0,0 +1,542 @@
+programming_languages = [
+    "Python",
+    "JavaScript",
+    "Java",
+    "C++",
+    "C#",
+    "Go",
+    "Ruby",
+    "Swift",
+    "TypeScript",
+    "Kotlin",
+    "Rust",
+    "PHP",
+    "R",
+    "Perl",
+    "Scala",
+    "Haskell",
+    "Lua",
+    "Shell scripting languages (e.g., Bash)",
+    "Dart",
+    "Objective-C",
+    "Clojure",
+    "Elixir",
+    "Elm",
+    "F#",
+    "Groovy",
+    "Julia",
+    "Nim",
+    "OCaml",
+    "Smalltalk",
+    "Tcl",
+    "Crystal",
+    "Pony",
+    "Chapel",
+    "Ballerina",
+    "COBOL",
+    "Fortran",
+    "Prolog",
+    "Scheme",
+    "Forth",
+    "Verilog"
+]
+web_development_frameworks = [
+    "HTML",
+    "CSS",
+    "JavaScript",
+    "React",
+    "Angular",
+    "Vue.js",
+    "Node.js",
+    "Express",
+    "Django",
+    "Ruby on Rails",
+    "Flask"
+]
+database_technologies = [
+    "SQL",
+    "MySQL",
+    "PostgreSQL",
+    "Oracle",
+    "MongoDB",
+    "Redis",
+    "Cassandra",
+    "SQLite",
+    "Microsoft SQL Server",
+    "IBM Db2",
+    "MariaDB",
+    "Amazon RDS",
+    "Google Cloud Spanner",
+    "Apache HBase",
+    "Couchbase",
+    "Neo4j",
+    "Amazon DynamoDB",
+    "Firebase Realtime Database",
+    "CockroachDB",
+    "Amazon Aurora"
+]
+networking_technologies = [
+    "Ethernet",
+    "Wi-Fi",
+    "Bluetooth",
+    "TCP/IP",
+    "DNS",
+    "DHCP",
+    "VPN",
+    "HTTP/HTTPS",
+    "SSL/TLS",
+    "IPv4/IPv6"
+]
+cloud_computing_technologies = [
+    "Amazon Web Services (AWS)",
+    "Microsoft Azure",
+    "Google Cloud Platform (GCP)",
+    "IBM Cloud",
+    "Oracle Cloud",
+    "Alibaba Cloud",
+    "VMware Cloud",
+    "OpenStack"
+]
+virtualization_technologies = [
+    "VMware",
+    "VirtualBox",
+    "Hyper-V",
+    "KVM",
+    "Xen",
+    "Docker"
+]
+cybersecurity_technologies = [
+    "Firewalls",
+    "Intrusion Detection Systems (IDS)",
+    "Intrusion Prevention Systems (IPS)",
+    "Antivirus software",
+    "Virtual Private Networks (VPNs)",
+    "Encryption technologies",
+    "Security Information and Event Management (SIEM) systems",
+    "Penetration testing tools (e.g., Metasploit, Nmap)",
+    "Web Application Firewalls (WAF)",
+    "Security assessment tools (e.g., Nessus, Qualys)",
+    "Endpoint security solutions"
+]
+devops_tools = [
+    "Jenkins",
+    "Ansible",
+    "Docker",
+    "Kubernetes",
+    "Terraform",
+    "Puppet",
+    "Chef",
+    "Prometheus",
+    "Grafana",
+    "ELK Stack"
+]
+ai_ml_frameworks = [
+    "TensorFlow",
+    "PyTorch",
+    "Keras",
+    "scikit-learn",
+    "MXNet",
+    "Caffe",
+    "Theano",
+    "Microsoft Cognitive Toolkit (CNTK)",
+    "H2O.ai",
+    "OpenAI Gym"
+]
+version_control_systems = [
+    "Git",
+    "Subversion (SVN)",
+    "Mercurial",
+    "Perforce",
+    "Team Foundation Version Control (TFVC)"
+]
+containerization_technologies = [
+    "Docker",
+    "Kubernetes",
+    "Podman",
+    "rkt",
+    "LXC"
+]
+frontend_frameworks = [
+    "React",
+    "Angular",
+    "Vue.js",
+    "Bootstrap",
+    "jQuery",
+    "Ember.js",
+    "Backbone.js",
+    "Svelte",
+    "Preact",
+    "Mithril"
+]
+backend_frameworks = [
+    "Django",
+    "Flask",
+    "Express.js",
+    "Spring",
+    "Ruby on Rails",
+    "Laravel",
+    "ASP.NET",
+    "Play Framework",
+    "NestJS",
+    "FastAPI"
+]
+mobile_platforms = [
+    "Android (Java/Kotlin)",
+    "iOS (Swift/Objective-C)",
+    "React Native",
+    "Xamarin",
+    "Flutter",
+    "Apache Cordova",
+    "Ionic",
+    "NativeScript",
+    "PhoneGap",
+    "Appcelerator Titanium"
+]
+testing_qa_tools = [
+    "Selenium",
+    "Jenkins",
+    "JUnit",
+    "Postman",
+    "Appium",
+    "SoapUI",
+    "Cucumber",
+    "TestNG",
+    "LoadRunner",
+    "Robot Framework"
+]
+project_management_tools = [
+    "Jira",
+    "Trello",
+    "Asana",
+    "Microsoft Project",
+    "Basecamp",
+    "Monday.com",
+    "Wrike",
+    "ClickUp",
+    "Smartsheet",
+    "Redmine"
+]
+data_visualization_tools = [
+    "Tableau",
+    "Power BI",
+    "QlikView",
+    "D3.js",
+    "Google Data Studio",
+    "Plotly",
+    "Looker",
+    "Highcharts",
+    "Sisense",
+    "Chart.js"
+]
+automation_tools = [
+    "Selenium",
+    "Jenkins",
+    "Ansible",
+    "Puppet",
+    "Chef",
+    "Terraform",
+    "Robot Framework",
+    "WinAppDriver",
+    "AutoIt",
+    "Cypress"
+]
+microservice_technologies = [
+    "Docker",
+    "Kubernetes",
+    "Amazon ECS",
+    "Red Hat OpenShift",
+    "Spring Boot",
+    "Node.js",
+    "gRPC",
+    "Istio",
+    "Linkerd",
+    "Consul"
+]
+serverless_platforms = [
+    "AWS Lambda",
+    "Azure Functions",
+    "Google Cloud Functions",
+    "IBM Cloud Functions",
+    "Alibaba Cloud Function Compute",
+    "Oracle Functions",
+    "Kubeless",
+    "OpenWhisk",
+    "Fission",
+    "IronFunctions"
+]
+erp_systems = [
+    "SAP ERP",
+    "Oracle ERP Cloud",
+    "Microsoft Dynamics 365",
+    "Infor ERP",
+    "NetSuite",
+    "IFS Applications",
+    "Epicor ERP",
+    "Sage Intacct",
+    "Workday",
+    "SAP S/4HANA"
+]
+cms_systems = [
+    "WordPress",
+    "Joomla",
+    "Drupal",
+    "Magento",
+    "Shopify",
+    "Squarespace",
+    "Wix",
+    "Weebly",
+    "TYPO3",
+    "Umbraco"
+]
+bi_analytics_tools = [
+    "Tableau",
+    "Power BI",
+    "QlikView",
+    "Google Analytics",
+    "IBM Cognos",
+    "Looker",
+    "Sisense",
+    "Domo",
+    "MicroStrategy",
+    "TIBCO Spotfire"
+]
+nosql_databases = [
+    "MongoDB",
+    "Cassandra",
+    "Couchbase",
+    "Redis",
+    "Amazon DynamoDB",
+    "Firebase",
+    "RethinkDB",
+    "RavenDB",
+    "MarkLogic",
+    "Cosmos DB"
+]
+ci_cd_tools = [
+    "Jenkins",
+    "Travis CI",
+    "CircleCI",
+    "GitLab CI",
+    "TeamCity",
+    "Bamboo",
+    "Azure DevOps",
+    "GitHub Actions",
+    "GoCD",
+    "Drone"
+]
+api_management_platforms = [
+    "Apigee",
+    "MuleSoft",
+    "IBM API Connect",
+    "AWS API Gateway",
+    "Microsoft Azure API Management",
+    "Google Cloud Endpoints",
+    "Kong",
+    "Tyk",
+    "WSO2 API Manager",
+    "3scale"
+]
+lowcode_nocode_platforms = [
+    "OutSystems",
+    "Appian",
+    "Microsoft Power Apps",
+    "Salesforce Lightning",
+    "Google App Maker",
+    "Mendix",
+    "Quick Base",
+    "Zoho Creator",
+    "Betty Blocks",
+    "Kissflow"
+]
+bigdata_platforms_tools = [
+    "Hadoop",
+    "Spark",
+    "Kafka",
+    "Hive",
+    "Presto",
+    "Flink",
+    "Cassandra",
+    "HBase",
+    "Druid",
+    "Databricks"
+]
+enterprise_messaging_systems = [
+    "RabbitMQ",
+    "Apache Kafka",
+    "ActiveMQ",
+    "IBM MQ",
+    "Amazon SNS",
+    "Google Cloud Pub/Sub",
+    "Microsoft Azure Service Bus",
+    "RocketMQ",
+    "NATS",
+    "Solace"
+]
+cdn_technologies = [
+    "Akamai",
+    "Cloudflare",
+    "Amazon CloudFront",
+    "Fastly",
+    "Limelight Networks"
+]
+graph_db_technologies = [
+    "Neo4j",
+    "Amazon Neptune",
+    "TigerGraph",
+    "ArangoDB",
+    "JanusGraph"
+]
+data_warehouse_technologies = [
+    "Amazon Redshift",
+    "Google BigQuery",
+    "Snowflake",
+    "Microsoft Azure Synapse Analytics",
+    "Oracle Exadata"
+]
+bpm_tools = [
+    "Pega",
+    "Appian",
+    "IBM Business Process Manager",
+    "Oracle BPM",
+    "Bizagi"
+]
+blockchain_technologies = [
+    "Ethereum",
+    "Hyperledger Fabric",
+    "Corda",
+    "Quorum",
+    "Stellar"
+]
+iam_solutions = [
+    "Okta",
+    "Azure Active Directory",
+    "Ping Identity",
+    "OneLogin",
+    "ForgeRock"
+]
+apm_solutions = [
+    "New Relic",
+    "Datadog",
+    "AppDynamics",
+    "Dynatrace",
+    "Splunk"
+]
+robotics_automation_technologies = [
+    "UiPath",
+    "Automation Anywhere",
+    "Blue Prism",
+    "Kofax",
+    "Pegasystems"
+]
+edge_computing_solutions = [
+    "AWS Greengrass",
+    "Microsoft Azure IoT Edge",
+    "Google Cloud IoT Edge",
+    "Dell Edge Gateway",
+    "Cisco Kinetic"
+]
+ar_vr_development_tools = [
+    "Unity",
+    "Unreal Engine",
+    "ARKit",
+    "Vuforia",
+    "Oculus SDK"
+]
+cognitive_computing_frameworks = [
+    "IBM Watson",
+    "Microsoft Cognitive Toolkit (CNTK)",
+    "Apache OpenNLP",
+    "TensorFlow",
+    "Keras"
+]
+dam_systems = [
+    "Adobe Experience Manager",
+    "Bynder",
+    "Widen Collective",
+    "Canto",
+    "Brandfolder"
+]
+data_science_platforms = [
+    "Anaconda",
+    "Databricks",
+    "Dataiku",
+    "RapidMiner",
+    "KNIME"
+]
+nlp_libraries_frameworks = [
+    "NLTK (Natural Language Toolkit)",
+    "SpaCy",
+    "Stanford NLP",
+    "Gensim",
+    "AllenNLP"
+]
+bioinformatics_tools = [
+    "Bioconductor",
+    "Genome Analysis Toolkit (GATK)",
+    "BLAST (Basic Local Alignment Search Tool)",
+    "Cytoscape",
+    "Biopython"
+]
+knowledge_management_systems = [
+    "Confluence",
+    "SharePoint",
+    "Notion",
+    "Bloomfire",
+    "Document360"
+]
+enterprise_search_solutions = [
+    "Elastic Enterprise Search",
+    "Apache Solr",
+    "Swiftype",
+    "Coveo",
+    "Algolia"
+]
+dlp_tools = [
+    "Symantec DLP",
+    "McAfee DLP",
+    "Digital Guardian",
+    "Forcepoint DLP",
+    "Code42"
+]
+ecommerce_platforms = [
+    "Shopify",
+    "Magento",
+    "WooCommerce",
+    "BigCommerce",
+    "Salesforce Commerce Cloud"
+]
+crm_systems = [
+    "Salesforce",
+    "HubSpot CRM",
+    "Microsoft Dynamics 365",
+    "Zoho CRM",
+    "Pipedrive"
+]
+scm_software = [
+    "SAP SCM",
+    "Oracle SCM",
+    "JDA Software",
+    "Kinaxis",
+    "Manhattan Associates"
+]
+emr_systems = [
+    "Epic Systems",
+    "Cerner",
+    "Allscripts",
+    "MEDITECH",
+    "NextGen Healthcare"
+]
+hrms_systems = [
+    "Workday",
+    "SAP SuccessFactors",
+    "Oracle HCM Cloud",
+    "BambooHR",
+    "ADP Workforce Now"
+]
+lms_systems = [
+    "Moodle",
+    "Canvas",
+    "Blackboard Learn",
+    "D2L Brightspace",
+    "Google Classroom"
+]
