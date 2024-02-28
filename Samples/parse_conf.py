31100a7a0766a5119b8d1297b33a03dab348de11
SnUSA <leha.aristar@gmail.com>
2024-02-29 00:54:38 +0300

    add comf

--- Samples/parse_conf.py
+++ Samples/parse_conf.py
@@ -4,7 +4,7 @@ import configparser
 def create_config():
     config = configparser.ConfigParser()
 
-    # Add sections and key-value pairs
+    # Добавление параметров конфигурации
     config['General'] = {'debug': True, 'log_level': 'info'}
     config['Database'] = {'db_name': 'example_db',
                           'db_host': 'localhost', 'db_port': '5432'}
