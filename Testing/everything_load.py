cc4b5687e1ec1b773f33882d07dccbf164ad2f44
pyramidheadshark <notevengael@gmail.com>
2024-02-29 17:05:14 +0300

    minor changes

---
+++ Testing/everything_load.py
@@ -0,0 +1,107 @@
+import json
+from datetime import datetime
+
+
+def calculate_work_duration(start_date, end_date):
+    start_date = datetime.strptime(start_date, '%Y-%m-%d')
+    end_date = datetime.strptime(end_date, '%Y-%m-%d')
+    duration = end_date - start_date
+    return duration.days / 365.2
+
+
+# Загрузка описания вакансии из JSON-файла
+
+def load_full_description():
+    with open('../Samples/obuchenie.json', 'r', encoding='utf-8') as f:
+        description_data = json.load(f)
+        return description_data
+
+
+def load_full_description_by_pos(description_data, pos):
+    return description_data[pos]
+
+
+def load_info_from_vacancy(description_data):
+    vacancy = description_data['vacancy']
+    uuid = vacancy['uuid']
+    keywords = vacancy['keywords']
+    description = vacancy['description']
+    return [uuid, keywords, description]
+
+
+def load_confirmed_resumes(full_description):
+    confirmed_resumes_data = full_description['confirmed_resumes']
+    return confirmed_resumes_data
+
+
+def load_failed_resumes(full_description):
+    failed_resumes_data = full_description['failed_resumes']
+    return failed_resumes_data
+
+
+def load_failed_resume_by_pos(failed_resumes_data, pos):
+    resume = failed_resumes_data[pos]
+    uuid = resume['uuid']
+    key_skills = resume['key_skills']
+    location = [resume['country'], resume['city']]
+    about = resume['about']
+    experienceItem = resume['experienceItem']
+    educationItem = resume['educationItem']
+    return [uuid, key_skills, location, about, define_experience_items(experienceItem),
+            define_education_items(educationItem)]
+
+
+def load_confirmed_resume_by_pos(confirmed_resumes_data, pos):
+    resume = confirmed_resumes_data[pos]
+    uuid = resume['uuid']
+    key_skills = resume['key_skills']
+    location = [resume['country'], resume['city']]
+    about = resume['about']
+    experienceItem = resume['experienceItem']
+    educationItem = resume['educationItem']
+    return [uuid, key_skills, location, about, define_experience_items(experienceItem),
+            define_education_items(educationItem)]
+
+
+def define_education_items(educationItem):
+    flag_high_education = False
+    count_courses = 0
+    for item in educationItem:
+        if item['education_level'] == 'Высшее':
+            flag_high_education = True
+        count_courses += 1
+    return [flag_high_education, count_courses]
+
+
+def define_experience_items(experienceItem):
+    count_years = float(0)
+    experience_descriptions = str()
+    for item in experienceItem:
+        if item['ends'] is not None:
+            count_years += calculate_work_duration(item['starts'], item['ends'])
+        else: count_years += calculate_work_duration(item['starts'], datetime.now().strftime('%Y-%m-%d'))
+        experience_descriptions.join(item['description'])
+
+    return [count_years, experience_descriptions]
+
+
+def final_everything_load():
+    final_resumes_base = []
+    description_data = load_full_description()
+    for i in range(len(description_data) - 1):
+        local_description_data = [load_info_from_vacancy(load_full_description_by_pos(description_data, i))]
+        failed_resumes_data = load_failed_resumes(load_full_description_by_pos(description_data, i))
+        confirmed_resumes_data = load_confirmed_resumes(load_full_description_by_pos(description_data, i))
+        local_resumes_data = []
+        for j_failed in range(len(failed_resumes_data) - 1):
+            print(j_failed, "failed")
+            local_resumes_data.append(load_failed_resume_by_pos(failed_resumes_data, j_failed))
+        for j_confirmed in range(len(confirmed_resumes_data) - 1):
+            local_resumes_data.append(load_confirmed_resume_by_pos(confirmed_resumes_data, j_confirmed))
+        local_description_data.append(local_resumes_data)
+        final_resumes_base.append(local_description_data)
+
+    return final_resumes_base
+
+
+print(final_everything_load())
--- Testing/nedodel_main_hueta.py
+++ Testing/nedodel_main_hueta.py
@@ -10,28 +10,29 @@ def calculate_work_duration(start_date, end_date):
     duration = end_date - start_date
     return duration
 
+
 # Загрузка описания вакансии из JSON-файла
 with open('../Samples/obuchenie.json', 'r', encoding='utf-8') as f:
     description_data = json.load(f)
     description = description_data[0]
 
-#json для проваленных ключевых умений
+
+# json для проваленных ключевых умений
 extracted_failed_data = []
-for i in range (len(job_description_data[0]["failed_resumes"])) :
+for i in range(len(job_description_data[0]["failed_resumes"])):
     extracted_failed_data.append(job_description_data[0]["failed_resumes"][i]["key_skills"])
 with open('extracted_failed_data.json', 'w', encoding='utf-8') as f:
     json.dump(extracted_failed_data, f, ensure_ascii=False, indent=4)
-    
-#json для успешных ключевых умений
-extracted_confirmed_data = []    
-for i in range (len(job_description_data[0]["confirmed_resumes"])) :
+
+# json для успешных ключевых умений
+extracted_confirmed_data = []
+for i in range(len(job_description_data[0]["confirmed_resumes"])):
     extracted_confirmed_data.append(job_description_data[0]["confirmed_resumes"][i]["key_skills"])
 with open('extracted_confirmed_data.json', 'w', encoding='utf-8') as f:
     json.dump(extracted_confirmed_data, f, ensure_ascii=False, indent=4)
 
-        
 print(job_description_data[0]["confirmed_resumes"][0]["key_skills"])
-   
+
 # Загрузка ключевых слов из другого JSON-файла
 with open('keywords.json', 'r', encoding='utf-8') as f:
     keywords_data = json.load(f)
@@ -41,14 +42,13 @@ with open('keywords.json', 'r', encoding='utf-8') as f:
 tokenizer = RobertaTokenizer.from_pretrained('roberta-base')
 model = RobertaForMaskedLM.from_pretrained('roberta-base')
 
-
-    # Токенизация описания вакансии
+# Токенизация описания вакансии
 inputs = tokenizer(job_description, return_tensors="pt", max_length=512, truncation=True)
 
-    # Токенизация ключевых слов
+# Токенизация ключевых слов
 keyword_ids = [tokenizer.encode(keyword, add_special_tokens=False) for keyword in keywords]
 
-    # Получение ближайших токенов для каждого ключевого слова
+# Получение ближайших токенов для каждого ключевого слова
 with torch.no_grad():
     outputs = model(**inputs)
     predictions = outputs.logits
@@ -59,7 +59,8 @@ for keyword_id in keyword_ids:
     for token_id in keyword_id:
         if token_id in inputs["input_ids"]:
             if tokenizer.mask_token_id in inputs["input_ids"]:
-                distance = abs(inputs["input_ids"].tolist().index(token_id) - inputs["input_ids"].tolist().index(tokenizer.mask_token_id))
+                distance = abs(inputs["input_ids"].tolist().index(token_id) - inputs["input_ids"].tolist().index(
+                    tokenizer.mask_token_id))
                 if distance < closest_distance:
                     closest_distance = distance
                     closest_token = token_id
@@ -71,13 +72,12 @@ predicted_words = tokenizer.convert_ids_to_tokens(closest_tokens)
 
 print(predicted_words)
 
- # доступ к описаниям у вакансии и резюме
+# доступ к описаниям у вакансии и резюме
 
-#print(job_description_data[0]["vacancy"])
-#print(job_description_data[0]["failed_resumes"][0]["experienceItem"][0]['description'])
-#print(job_description_data[0]["confirmed_resumes"][0]["experienceItem"][0]['description'])
-#первый индекс вакансия
-#второй индекс работник на данную вакансию
-#третий индекс описание к месту работу
-#встроенные циклы вам в помощь, увидимся в 8, жаль что код хуйня полная.
-
+# print(job_description_data[0]["vacancy"])
+# print(job_description_data[0]["failed_resumes"][0]["experienceItem"][0]['description'])
+# print(job_description_data[0]["confirmed_resumes"][0]["experienceItem"][0]['description'])
+# первый индекс вакансия
+# второй индекс работник на данную вакансию
+# третий индекс описание к месту работу
+# встроенные циклы вам в помощь, увидимся в 8, жаль что код хуйня полная.
